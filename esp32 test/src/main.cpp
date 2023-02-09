34182c45451d4644515e71e61be6389d14de33d4
abarbot <abarbot@btslivh.eu>
2023-02-09 17:16:03 +0100

    test led esp32

---
+++ esp32 test/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define LED 2
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);// put your setup code here, to run once:
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(LED,HIGH);
+  Serial.println("LED ON");
+  delay(500);
+  digitalWrite(LED,LOW);
+  Serial.println("Led off");// put your main code here, to run repeatedly:
+}
