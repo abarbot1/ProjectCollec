0a01910a577a99b08e3e42c37fe0ca821225212f
smunoz4 <smunoz@btslivh.eu>
2023-02-13 11:34:45 +0100

    Ajout code connexion wifi Acces_Wifi_ESP32

---
+++ Acces_Wifi_ESP32/src/main.cpp
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+const char* ssid = "yourNetworkName";
+const char* password = "yourNetworkPassword";
+
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop(){}
