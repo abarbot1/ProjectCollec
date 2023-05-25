1aee2209ac3cddf9563f24e0d93d2c8e299edb62
abarbot <abarbot@btslivh.eu>
2023-05-25 16:36:32 +0200

    dernière version avec prof

--- thread/src/main.cpp
+++ thread/src/main.cpp
@@ -5,121 +5,86 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
-#include <string.h>
+#include <queue>
+#include "LoRa.h"
+
 using namespace std;
 
-mutex SPIMutex; // déclaration du mutex pour protéger l'accès aux données de la voie SPI
-const int PACKET_HEADER_SIZE = 11;
+mutex mutexQueue; // déclaration du mutex pour protéger l'accès aux données de la voie SPI
+queue<char *> tramesRecues;
 // Fonction thread pour la lecture de données sur la voie SPI
-void SPIReadThread(int spi_fd)
+void *rx_f(void *p)
 {
-    while (true)
-    {
-        char buf[1024];
-        int n = read(spi_fd, buf, sizeof(buf)); // lecture de données sur la voie SPI
-        if (n > 0)
-        {
-            lock_guard<mutex> lock(SPIMutex); // verrouillage du mutex
-            // affichage des données reçues
-            cout << "Received " << n << " bytes: ";
-            for (int i = 0; i < n; i++)
-                cout << buf[i];
-            cout << endl;
-        }
-    }
+    rxData *rx = (rxData *)p;
+    printf("rx done \n");
+    printf("CRC error: %d\n", rx->CRC);
+    printf("string: %s\n", rx->buf); // Data we'v received
+    printf("RSSI: %d\n", rx->RSSI);
+    printf("SNR: %f\n", rx->SNR);
+    free(p);
+    lock_guard<mutex> lock(mutexQueue);
+    tramesRecues.push(rx->buf);
+    return NULL;
+}
+
+void LoRaRx()
+{
+    LoRa_ctl modem;
+
+    // See for typedefs, enumerations and there values in LoRa.h header file
+    modem.spiCS = 0; // Raspberry SPI CE pin number
+    modem.rx.callback = rx_f;
+    modem.eth.payloadLen = 5; // payload len used in implicit header mode
+    modem.eth.preambleLen = 6;
+    modem.eth.bw = BW62_5;            // Bandwidth 62.5KHz
+    modem.eth.sf = SF12;              // Spreading Factor 12
+    modem.eth.ecr = CR8;              // Error coding rate CR4/8
+    modem.eth.CRC = 1;                // Turn on CRC checking
+    modem.eth.freq = 434800000;       // 434.8MHz
+    modem.eth.resetGpioN = 4;         // GPIO4 on lora RESET pi
+    modem.eth.dio0GpioN = 17;         // GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
+    modem.eth.outPower = OP20;        // Output power
+    modem.eth.powerOutPin = PA_BOOST; // Power Amplifire pin
+    modem.eth.AGC = 1;                // Auto Gain Control
+    modem.eth.OCP = 240;              // 45 to 240 mA. 0 to turn off protection
+    modem.eth.implicitHeader = 1;     // Implicit header mode
+    modem.eth.syncWord = 0x12;
+    // For detail information about SF, Error Coding Rate, Explicit header, Bandwidth, AGC, Over current protection and other features refer to sx127x datasheet https://www.semtech.com/uploads/documents/DS_SX1276-7-8-9_W_APP_V5.pdf
+
+    LoRa_begin(&modem);
+    LoRa_receive(&modem);
+
+    sleep(60);
+    printf("end\n");
+    LoRa_end(&modem);
 }
 
 // Fonction thread pour l'analyse des données de la voie SPI
-void SPIAnalyzeThread(int spi_fd)
+void traitementTrame()
 {
-    char buf[1024];
-    int pos = 0;
-    int len = 0;
-
     while (true)
     {
-        lock_guard<mutex> lock(SPIMutex); // verrouillage du mutex
-
-        // Lire les données de la voie SPI
-        int n = read(spi_fd, buf+pos, sizeof(buf)-pos);
-
-        if (n <= 0)
-            continue;
-
-        // Mettre à jour la position du curseur de lecture
-        pos += n;
-
-        // Vérifier si nous avons une en-tête de paquet complète
-        if (pos >= PACKET_HEADER_SIZE)
+        lock_guard<mutex> lock(mutexQueue); // verrouillage du mutex
+        if (!tramesRecues.empty())
         {
-            // Analyser l'en-tête de paquet
-            int packetLen = buf[0];
-            int packetChecksum = buf[1];
-
-            // Vérifier si nous avons un paquet complet
-            if (pos >= packetLen)
-            {
-                // Vérifier le checksum
-                int checksum = 0;
-                for (int i = 0; i < packetLen; i++)
-                    checksum += buf[i];
-
-                if (checksum == packetChecksum)
-                {
-                    // Extraire le message du paquet
-                    string message(buf + PACKET_HEADER_SIZE, packetLen - PACKET_HEADER_SIZE);
-                    cout << "Received message: " << message << endl;
-
-                    // Mettre à jour la position du curseur de lecture
-                    pos -= packetLen;
-
-                    // Décaler les données restantes vers le début du tampon
-                    if (pos > 0)
-                        memmove(buf, buf+packetLen, pos-packetLen);
-                }
-                else
-                {
-                    // Le checksum est incorrect, abandonner le paquet
-                    pos = 0;
-                }
-            }
+            cout << "traitement de la dernière trame reçue " << endl;
+            cout << tramesRecues.front();
+            tramesRecues.pop(); 
         }
     }
 }
 
 int main()
 {
-    // Ouverture de la voie SPI
-    int spi_fd = open("/dev/spidev0.0", O_RDWR);
-    if (spi_fd < 0)
-    {
-        cout << "Failed to open SPI device." << endl;
-        return 1;
-    }
-
-    // Configuration de la voie SPI
-    uint8_t mode = SPI_MODE_0;
-    uint8_t bits = 8;
-    uint32_t speed = 1000000;
-    int ret = ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
-    ret |= ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-    ret |= ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-    if (ret < 0)
-    {
-        cout << "Failed to configure SPI device." << endl;
-        return 1;
-    }
 
     // Démarrage des threads de lecture et d'analyse de données
-    thread readThread(SPIReadThread, spi_fd);
-    thread analyzeThread(SPIAnalyzeThread);
+    thread readThread(LoRaRx);
+    thread analyzeThread(traitementTrame);
 
     // Attendre jusqu'à ce que l'utilisateur appuie sur une touche, puis terminer les threads et fermer la voie SPI
     cout << "Press ctrl+c to stop." << endl;
     getchar();
-    close(spi_fd);
     readThread.join();
     analyzeThread.join();
-
     return 0;
 }
